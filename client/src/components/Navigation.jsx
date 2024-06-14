7742dba628f4ac3d1f96db10d49ac2645d6e8852
Damyan Dimov <dam.dimov99@gmail.com>
2024-06-14 21:07:37 +0300

    first commit

---
+++ client/src/components/Navigation.jsx
@@ -0,0 +1,14 @@
+export default function Navigation() {
+    return (
+        <div>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Dashboard</a></li>
+                <li><a href="#">Login</a></li>
+                <li><a href="#">Register</a></li>
+                <li><a href="#">Logout</a></li>
+            </ul>
+        </div>
+    )
+}
+
