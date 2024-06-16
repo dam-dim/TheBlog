86c9d65a85401029b91794432a045415d00518c8
Damyan Dimov <dam.dimov99@gmail.com>
2024-06-16 20:57:00 +0300

    login component

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,16 +1,32 @@
 import styles from "./Login.module.css";
 
 export default function Login() {
+    const loginHandler = (e) => {
+        e.preventDefault();
+    };
+
     return (
         <div className={styles.login}>
-            <h2>Login</h2>
-            <form action="">
-                <label htmlFor="username">Username: </label>
-                <input type="text" name="username" id="username" />
+            <form action="" method="POST" onSubmit={loginHandler}>
+                <h1>Log in</h1>
+                <div className={styles.username}>
+                    <label htmlFor="username">Username</label>
+                    <input type="text" name="username" id="username" />
+                </div>
 
-                <label htmlFor="password">Password: </label>
-                <input type="password" name="password" id="password" />
+                <div className={styles.password}>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="password" />
+                </div>
+
+                <div className={styles.submit}>
+                    <input type="submit" id="submit" value="Log in" />
+                </div>
             </form>
+            <p>
+                If You do not have an account, You can register{" "}
+                <a href="/register">here.</a>
+            </p>
         </div>
     );
 }
