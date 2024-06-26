52c9c05e97efbec04d424fbd58a8fc11a83a9071
Damyan Dimov <dam.dimov99@gmail.com>
2024-06-26 08:26:18 +0300

    provided auth for route paths

--- client/src/components/main/Main.jsx
+++ client/src/components/main/Main.jsx
@@ -12,20 +12,31 @@ import Create from "../create/Create";
 import Edit from "../edit/Edit";
 import Fill from "../fill/Fill";
 import Logout from "../logout/Logout";
+import { useContext } from "react";
+import AuthContext from "../../contexts/authContext";
 
 export default function Main() {
+    const { auth } = useContext(AuthContext);
     return (
         <div className={styles.main}>
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
                 <Route path="/posts/:postId/details" element={<Details />} />
-                <Route path="/create" element={<Create />} />
-                <Route path="/edit" element={<Edit />} />
-                <Route path="/fill" element={<Fill />} />
-                <Route path="/logout" element={<Logout />} />
+                {auth.token !== "" ? (
+                    <>
+                        <Route path="/dashboard" element={<Dashboard />} />
+                        <Route path="/create" element={<Create />} />
+                        <Route path="/edit" element={<Edit />} />
+                        <Route path="/fill" element={<Fill />} />
+                        <Route path="/logout" element={<Logout />} />
+                    </>
+                ) : (
+                    <>
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/register" element={<Register />} />
+                    </>
+                )}
+
                 <Route path="*" element={<PathError />} />
             </Routes>
         </div>
