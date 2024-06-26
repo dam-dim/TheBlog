47590a7df5f0b64231c78e438fcd861ebc089bc8
Damyan Dimov <dam.dimov99@gmail.com>
2024-06-26 09:26:59 +0300

    abstract request methods

---
+++ client/src/utils/request.js
@@ -0,0 +1,47 @@
+/**
+ *
+ * @param {*} url
+ * @param {*} method
+ * @param {*} data
+ * @returns
+ * @throws
+ */
+const request = async (url, method, data) => {
+    const response = await fetch(url, { ...getOptions(data), method });
+
+    const result = await response.json();
+
+    if (!response.ok) {
+        throw result;
+    }
+
+    return result;
+};
+
+/**
+ *
+ * @param {*} data
+ * @returns options
+ */
+const getOptions = (data) => {
+    const options = {};
+
+    if (data) {
+        options.headers = { "Content-Type": "application/json" };
+        options.body = JSON.stringify(data);
+    }
+
+    const token = localStorage.getItem("token");
+
+    if (token) {
+        options.headers = { ...options.headers, "X-Authorization": token };
+    }
+
+    return options;
+};
+
+export const get = (url) => request(url, "GET");
+export const post = (url, data) => request(url, "POST", data);
+export const remove = (url) => request(url, "DELETE");
+export const put = (url, data) => request(url, "PUT", data);
+export const patch = (url, data) => request(url, "PATCH", data);
