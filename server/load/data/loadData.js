6272b91d750e175e206c509c79e6d790786ab0f8
Damyan Dimov <dam.dimov99@gmail.com>
2024-07-28 14:59:42 +0300

    added getComments to initial data loading in the server

--- server/load/data/loadData.js
+++ server/load/data/loadData.js
@@ -1,8 +1,12 @@
+const getCategories = require("./categories/getCategories");
+const getComments = require("./comments/getComments");
 const getPosts = require("./posts/getPosts");
 
 function loadData() {
     const output = {
         posts: getPosts(),
+        // categories: getCategories(),
+        comments: getComments(),
     };
 
     return output;
