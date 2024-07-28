738dab650eb3822dfbf41a65c504af0531722af6
Damyan Dimov <dam.dimov99@gmail.com>
2024-07-28 17:41:45 +0300

    Update RecentListItem.jsx
    getting the category for each post instead of the category _id

--- client/src/components/home/recent-list/recent-list-item/RecentListItem.jsx
+++ client/src/components/home/recent-list/recent-list-item/RecentListItem.jsx
@@ -1,14 +1,27 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import styles from "./RecentListItem.module.css";
+
 import parseDate from "../../../../utils/dateParser";
+import * as categoryService from "../../../../services/categoryService";
+
+import styles from "./RecentListItem.module.css";
 
 export default function RecentListItem(props) {
+    const [category, setCategory] = useState("");
+
+    useEffect(() => {
+        categoryService
+            .getCategoryById(props.category)
+            .then((res) => setCategory(res[0]))
+            .catch((err) => console.log(err));
+    }, []);
+
     return (
         <div className={styles.card}>
             <img src={props?.imageUrl} alt="" />
             <div className={styles.cardDetails}>
                 <h3>{props.title}</h3>
-                <p>{props.category}</p>
+                <p>{category.name}</p>
 
                 <div className={styles.creator}>
                     <p>@{props.author?.username}</p>
