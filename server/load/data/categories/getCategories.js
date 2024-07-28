34eb6ddb01d89b9c4ce478057ec612d24575bd21
Damyan Dimov <dam.dimov99@gmail.com>
2024-07-28 16:56:58 +0300

    Update getCategories.js
    set of categories on intial start of the server

--- server/load/data/categories/getCategories.js
+++ server/load/data/categories/getCategories.js
@@ -1,9 +1,33 @@
 function getCategories() {
     const output = {
-        //
+        "da8eccc2-a7f1-4bc6-88ca-f9dfe6ead16e": {
+            _ownerId: "847ec027-f659-4086-8032-5173e2f9c93a",
+            _createdOn: 1722173574096,
+            name: "Lifestyle",
+        },
+        "9b778e93-1a31-43c4-b03f-7dd74aa60a6c": {
+            _ownerId: "847ec027-f659-4086-8032-5173e2f9c93a",
+            _createdOn: 1722173581733,
+            name: "Food & Drink",
+        },
+        "67596660-ab3c-43a6-bb9b-e2a40a7f483f": {
+            _ownerId: "847ec027-f659-4086-8032-5173e2f9c93a",
+            _createdOn: 1722173587463,
+            name: "Technology",
+        },
+        "692452eb-3917-44f0-b739-c92333b0a1d5": {
+            _ownerId: "847ec027-f659-4086-8032-5173e2f9c93a",
+            _createdOn: 1722173593603,
+            name: "Business & Finance",
+        },
+        "b82fa049-065b-4e15-b8d9-db4bc8bc793d": {
+            _ownerId: "847ec027-f659-4086-8032-5173e2f9c93a",
+            _createdOn: 1722173922643,
+            name: "Other",
+        },
     };
 
     return output;
 }
 
-module.exports = getCategories();
+module.exports = getCategories;
