99c115e26167a88526cc9ea13ba0943cebf6b2ba
Damyan Dimov <dam.dimov99@gmail.com>
2024-07-28 16:58:14 +0300

    Update loadUsers.js
    changed one user to be admin

--- server/load/users/loadUsers.js
+++ server/load/users/loadUsers.js
@@ -1,14 +1,14 @@
 function loadUsers() {
     let output = {
         "35c62d76-8152-4626-8712-eeb96381bea8": {
-            email: "peter@abv.bg",
+            email: "peter@gabv.bg",
             username: "peter23",
             hashedPassword:
                 "83313014ed3e2391aa1332615d2f053cf5c1bfe05ca1cbcb5582443822df6eb1",
         },
         "847ec027-f659-4086-8032-5173e2f9c93a": {
-            email: "george@abv.bg",
-            username: "george.me",
+            email: "admin@gmail.com",
+            username: "admin",
             hashedPassword:
                 "83313014ed3e2391aa1332615d2f053cf5c1bfe05ca1cbcb5582443822df6eb1",
         },
