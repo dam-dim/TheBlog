e66608bdb91150c085e7b0f719eafb2dbefd5d4e
Damyan Dimov <dam.dimov99@gmail.com>
2024-07-28 15:22:38 +0300

    Update Comment.jsx
    dynamic rendering of data for comment

--- client/src/components/comments/comment/Comment.jsx
+++ client/src/components/comments/comment/Comment.jsx
@@ -1,26 +1,23 @@
-import styles from "./Comment.module.css";
+import { useEffect } from "react";
 
-export default function Comment() {
+import styles from "./Comment.module.css";
+import parseDate from "../../../utils/dateParser";
+
+export default function Comment(props) {
     return (
         <div className={styles.comment}>
-            <div className={styles.commentImage}>
+            {/* <div className={styles.commentImage}>
                 <img
                     src="https://static.vecteezy.com/system/resources/thumbnails/002/318/271/small_2x/user-profile-icon-free-vector.jpg"
                     alt=""
                 />
-            </div>
+            </div> */}
 
             <div className={styles.commentContent}>
-                <h4>@username on June 16, 2024</h4>
-                <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    Dolorem doloribus libero illum dicta voluptate magni saepe
-                    veritatis tempore illo esse veniam a vel magnam, rerum
-                    beatae cum incidunt perspiciatis. Veritatis ex totam
-                    possimus. Dicta dolorum eligendi maxime modi exercitationem
-                    quibusdam culpa reiciendis alias iste officiis cumque, optio
-                    voluptates non nam!
-                </p>
+                <h4>
+                    @{props.username} on {parseDate(props.createdOn)}
+                </h4>
+                <p>{props.content}</p>
             </div>
         </div>
     );
