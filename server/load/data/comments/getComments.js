ddafa87b7075ba2a787611bd0353847a60b94452
Damyan Dimov <dam.dimov99@gmail.com>
2024-07-28 14:59:11 +0300

    Create getComments.js

---
+++ server/load/data/comments/getComments.js
@@ -0,0 +1,20 @@
+function getComments() {
+    const output = {
+        "7e4b4867-e87d-4f61-af56-d249e854d858": {
+            _ownerId: "60f0cf0b-34b0-4abd-9769-8c42f830dffc",
+            _createdOn: 1722166037350,
+            postId: "1e6d9aa9-1e68-494a-a2e3-dfd1aeacba69",
+            content: "asdsadsa",
+        },
+        "df6444c7-b4da-40c0-9ce8-c58ef8909d65": {
+            _ownerId: "60f0cf0b-34b0-4abd-9769-8c42f830dffc",
+            _createdOn: 1722166227828,
+            postId: "1e6d9aa9-1e68-494a-a2e3-dfd1aeacba69",
+            content: "asdsadsa",
+        },
+    };
+
+    return output;
+}
+
+module.exports = getComments;
