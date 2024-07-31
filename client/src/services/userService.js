f029be970dd4041627edc1a4b7339933f6fd83a0
Damyan Dimov <dam.dimov99@gmail.com>
2024-07-31 22:36:54 +0300

    refactored some functions and added a new one

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -1,26 +1,26 @@
 import * as request from "../lib/request";
+import { getPostsCountByAuthorId } from "./postService";
 
 const BASE_URL = "http://localhost:3030/users";
-const DATA_URL = "http://localhost:3030/data/users";
 
-export const register = async (payload) => {
-    try {
-        const result = await request.post(`${BASE_URL}/register`, payload);
-        return result;
-    } catch (error) {
-        throw new Error(error.message);
-    }
-};
+export const register = (payload) =>
+    request.post(`${BASE_URL}/register`, payload);
 
-export const login = async (payload) => {
-    try {
-        const result = await request.post(`${BASE_URL}/login`, payload);
-        return result;
-    } catch (error) {
-        throw new Error(error.message);
-    }
-};
+export const login = (payload) => request.post(`${BASE_URL}/login`, payload);
 
 export const logout = () => request.get(`${BASE_URL}/logout`);
 
 export const me = () => request.get(`${BASE_URL}/me`);
+
+export const getCurrentUserData = async () => {
+    let currentUser = {};
+
+    try {
+        currentUser = await request.get(`${BASE_URL}/me`);
+        currentUser.postsCount = await getPostsCountByAuthorId(currentUser._id);
+
+        return currentUser;
+    } catch (error) {
+        throw error;
+    }
+};
