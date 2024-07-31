3121408d476f9a0411b5859c90b310926d3a368c
Damyan Dimov <dam.dimov99@gmail.com>
2024-07-31 23:26:33 +0300

    add first last name to persistedstate hook

--- client/src/hooks/usePersistedState.js
+++ client/src/hooks/usePersistedState.js
@@ -3,25 +3,31 @@ import { useState } from "react";
 export default function usePersistedState(initialValue) {
     const [auth, setAuth] = useState(() => {
         const current = {
-            token: localStorage.getItem("token"),
             username: localStorage.getItem("username"),
+            firstName: localStorage.getItem("firstName"),
+            lastName: localStorage.getItem("lastName"),
             email: localStorage.getItem("email"),
+            token: localStorage.getItem("token"),
         };
 
         return current.token ? current : initialValue;
     });
 
-    const setCurrentUser = (username, email, token) => {
-        setAuth({ username, email, token });
-        localStorage.setItem("token", token);
+    const setCurrentUser = (username, firstName, lastName, email, token) => {
+        setAuth({ username, firstName, lastName, email, token });
         localStorage.setItem("username", username);
+        localStorage.setItem("firstName", firstName);
+        localStorage.setItem("lastName", lastName);
         localStorage.setItem("email", email);
+        localStorage.setItem("token", token);
     };
 
     const removeCurrentUser = () => {
         setAuth({});
         localStorage.removeItem("token");
         localStorage.removeItem("username");
+        localStorage.removeItem("firstName");
+        localStorage.removeItem("lastName");
         localStorage.removeItem("email");
     };
 
