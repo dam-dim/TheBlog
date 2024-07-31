5b128d8e054939fe683687765038f6c72de420ce
Damyan Dimov <dam.dimov99@gmail.com>
2024-07-31 23:25:39 +0300

    added first and last name to the initial recourse

--- server/load/users/loadUsers.js
+++ server/load/users/loadUsers.js
@@ -2,18 +2,24 @@ function loadUsers() {
     let output = {
         "35c62d76-8152-4626-8712-eeb96381bea8": {
             email: "peter@gabv.bg",
+            firstName: "Peter",
+            lastName: "Petrov",
             username: "peter23",
             hashedPassword:
                 "83313014ed3e2391aa1332615d2f053cf5c1bfe05ca1cbcb5582443822df6eb1",
         },
         "847ec027-f659-4086-8032-5173e2f9c93a": {
             email: "admin@gmail.com",
+            firstName: "admin",
+            lastName: "admin",
             username: "admin",
             hashedPassword:
                 "83313014ed3e2391aa1332615d2f053cf5c1bfe05ca1cbcb5582443822df6eb1",
         },
         "60f0cf0b-34b0-4abd-9769-8c42f830dffc": {
             email: "dam@gmail.com",
+            firstName: "Damyan",
+            lastName: "Dimov",
             username: "damdim",
             hashedPassword:
                 "83313014ed3e2391aa1332615d2f053cf5c1bfe05ca1cbcb5582443822df6eb1",
