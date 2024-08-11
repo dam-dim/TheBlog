4692aefc0b7c347ca878e1fb1baeb2e6cd5c27f3
Damyan Dimov <dam.dimov99@gmail.com>
2024-08-11 15:47:19 +0300

    updated category component and its css

--- client/src/components/category-view/category-view-post/CategoryViewPost.jsx
+++ client/src/components/category-view/category-view-post/CategoryViewPost.jsx
@@ -1,9 +1,34 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import parseDate from "../../../utils/dateParser";
+import * as categoryService from "../../../services/categoryService";
+
 import styles from "./CategoryViewPost.module.css";
 
 export default function CategoryViewPost(props) {
+    const [category, setCategory] = useState("");
+
+    useEffect(() => {
+        categoryService
+            .getCategoryById(props.category)
+            .then((res) => setCategory(res[0]))
+            .catch((err) => console.log(err));
+    }, []);
+
     return (
-        <div>
-            <h1>{props.post.title}</h1>
+        <div className={styles.card}>
+            <img src={props?.imageUrl} alt="" />
+            <div className={styles.cardDetails}>
+                <h4>{props.title}</h4>
+                <p>{category.name}</p>
+
+                <div className={styles.creator}>
+                    <p>@{props.author?.username}</p>
+                    <p>{parseDate(props._createdOn)}</p>
+                </div>
+                <Link to={`/posts/${props._id}/details`}>Read more</Link>
+            </div>
         </div>
     );
 }
