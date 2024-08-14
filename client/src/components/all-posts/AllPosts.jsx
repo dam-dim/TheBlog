a14d9af9b6fa90bfbe956fbc3d61eaa0bb0a3c88
Damyan Dimov <dam.dimov99@gmail.com>
2024-08-14 22:28:03 +0300

    Update AllPosts.jsx

--- client/src/components/all-posts/AllPosts.jsx
+++ client/src/components/all-posts/AllPosts.jsx
@@ -3,13 +3,13 @@ import usePagination from "../../hooks/usePagination";
 import styles from "./AllPosts.module.css";
 
 import Filters from "./filters/Filters";
-import Post from "./post/Post";
+import PostV2 from "./post2/PostV2";
 import Pagination from "../pagination/Pagination";
 import useFilters from "../../hooks/useFilters";
 import { useEffect } from "react";
 
 const START_PAGE = 1;
-const POSTS_PER_PAGE = 10;
+const POSTS_PER_PAGE = 8;
 
 export default function AllPosts() {
     const {
@@ -27,9 +27,15 @@ export default function AllPosts() {
 
             <Filters filters={filters} onChange={onChangeFilter} />
 
+            <Pagination
+                currentPage={currentPage}
+                decreasePageNumber={decreasePageNumber}
+                increasePageNumber={increasePageNumber}
+            />
+
             <div className={styles.allWrapper}>
                 {posts.map((post) => {
-                    return <Post key={post._id} {...post} />;
+                    return <PostV2 key={post._id} {...post} />;
                 })}
             </div>
 
