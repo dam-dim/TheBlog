87adfd7975ccf0db1c34ce6abee674911c960e37
Damyan Dimov <dam.dimov99@gmail.com>
2024-08-14 22:28:15 +0300

    Update Fill.jsx

--- client/src/components/admin/fill/Fill.jsx
+++ client/src/components/admin/fill/Fill.jsx
@@ -1,17 +1,20 @@
 import * as postService from "../../../services/postService";
 import styles from "./Fill.module.css";
 
-export default function Fill() {
+export default function Fill(props) {
     const onClickHandler = async () => {
         try {
+            alert("Are you sure?");
             await postService.fill();
+            props.updateState();
         } catch (error) {
             console.log(error);
         }
     };
+
     return (
         <div className={styles.fill} onClick={onClickHandler}>
-            <h1>FILL THE DB WITH DUMMY POSTS</h1>
+            <h1>ADD 100 DUMMY POSTS</h1>
         </div>
     );
 }
