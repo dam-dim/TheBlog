cf3160b2f2296607e99bee4255f22f1dcfa8fb50
Damyan Dimov <dam.dimov99@gmail.com>
2024-08-14 22:28:25 +0300

    Update Admin.jsx

--- client/src/components/admin/Admin.jsx
+++ client/src/components/admin/Admin.jsx
@@ -5,14 +5,16 @@ import * as categoryService from "../../services/categoryService";
 import Input from "../form/input/Input";
 import Submit from "../form/submit/Submit";
 import useForm from "../../hooks/useForm";
-import { getPostsCountByCategoryId } from "../../services/postService";
+import parseDate from "../../utils/dateParser";
 
 const initialValues = {
     category: "",
+    submit: "",
 };
 
 export default function Admin() {
     const [categories, setCategories] = useState([]);
+    const [render, setRender] = useState(0);
     const { formValues, fieldErrors, onChange, onBlur, onSubmit } = useForm(
         submitHandler,
         initialValues
@@ -23,49 +25,76 @@ export default function Admin() {
             .getAllAndSetPostsCount()
             .then(setCategories)
             .catch((err) => console.log(err));
-    }, []);
+    }, [render]);
 
     async function submitHandler(values) {
-        const isUnique = await categoryService.isUnique(values.category);
+        const newCategory = await categoryService.create(values.category);
 
-        if (isUnique) {
-            const newCategory = await categoryService.create(values.category);
+        newCategory.postsCount = 0;
 
-            setCategories((prevState) => {
-                return [...prevState, newCategory];
-            });
-        }
+        setCategories((prevState) => {
+            return [...prevState, newCategory];
+        });
     }
+
+    const updateState = () => {
+        setRender((state) => !state);
+    };
 
     return (
         <div className={styles.admin}>
             <h1>Admin Panel</h1>
 
-            <Fill />
+            <div className={styles.categories}>
+                <h2>Categories</h2>
+                <div className={styles.categoriesWrapper}>
+                    <table>
+                        <thead>
+                            <tr>
+                                <td>Name</td>
+                                <td>Posts</td>
+                                <td>Created On</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {categories.map((currCategory) => (
+                                <tr
+                                    className={styles.category}
+                                    key={currCategory._id}
+                                >
+                                    <td>{currCategory.name}</td>
+                                    <td>{currCategory.postsCount}</td>
+                                    <td>
+                                        {parseDate(currCategory._createdOn)}
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
 
-            <div>
-                {categories.map((currCategory) => (
-                    <div key={currCategory._id}>
-                        <p>Name: {currCategory.name}</p>
-                        <p>Posts in that category: {currCategory.postsCount}</p>
-                        <p>Created: {currCategory._createdOn}</p>
-                        <br></br>
-                    </div>
-                ))}
+                <div className={styles.addCategory}>
+                    <h3>Add a category</h3>
+
+                    <form className={styles.form} onSubmit={onSubmit}>
+                        <Input
+                            id="category"
+                            value={formValues.category}
+                            onChange={onChange}
+                            onBlur={onBlur}
+                            error={fieldErrors.category}
+                            class={styles.input}
+                        />
+                        <Submit
+                            class={styles.submit}
+                            error={fieldErrors.submit}
+                            buttonText="Create"
+                        />
+                    </form>
+                </div>
             </div>
 
-            <div>
-                <form onSubmit={onSubmit}>
-                    <Input
-                        id="category"
-                        value={formValues.category}
-                        onChange={onChange}
-                        onBlur={onBlur}
-                        error={fieldErrors.category}
-                    />
-                    <Submit error={fieldErrors.submit} buttonText="Create" />
-                </form>
-            </div>
+            <Fill updateState={updateState} />
         </div>
     );
 }
