8133068b95f7ad784c0b056a296dc4b54356854c
Damyan Dimov <dam.dimov99@gmail.com>
2024-08-15 10:29:23 +0300

    added util function for the error log

---
+++ client/src/utils/logger.js
@@ -0,0 +1,14 @@
+/**
+ *
+ * @param error An error object
+ */
+const logErrors = (error) => {
+    // Here we can do whatever with the error object
+    //  - check for messages
+    //  - write to a file not the browser
+    //  - etc.
+
+    console.log(error);
+};
+
+export default logErrors;
