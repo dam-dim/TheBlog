20cc63ad33425d61589c99e727ce697fd525d88c
Damyan Dimov <dam.dimov99@gmail.com>
2024-08-15 10:30:02 +0300

    Update MyError.jsx

--- client/src/components/myError/MyError.jsx
+++ client/src/components/myError/MyError.jsx
@@ -1,16 +1,11 @@
 import styles from "./MyError.module.css";
 
-export default function MyError({ messages }) {
+export default function MyError() {
     return (
-        <div className={styles.myErrors}>
-            {messages &&
-                messages.map((message) => {
-                    return (
-                        <div className={styles.myError} key={message}>
-                            <h4>{message}</h4>
-                        </div>
-                    );
-                })}
+        <div className={styles.myError}>
+            <h1>An error occured!</h1>
+            <h2>We are doing our best to fix the problem!</h2>
+            <h2>Feel free to use the rest of the website!</h2>
         </div>
     );
 }
