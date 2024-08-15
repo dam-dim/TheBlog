851b61c13ee38a0ce956770cccf82ebd2ee75314
Damyan Dimov <dam.dimov99@gmail.com>
2024-08-15 10:29:50 +0300

    combined 2 use effects and added an error log

--- client/src/hooks/usePagination.js
+++ client/src/hooks/usePagination.js
@@ -2,29 +2,37 @@ import { useEffect, useState } from "react";
 
 import * as postService from "../services/postService";
 import useFilters from "./useFilters";
+import { useNavigate } from "react-router-dom";
+import logErrors from "../utils/logger";
 
 export default function usePagination(initialPage, postsPerPage) {
     const [currentPage, setCurrentPage] = useState(initialPage);
     const [posts, setPosts] = useState([]);
     const [allPostsCount, setAllPostsCount] = useState(0);
     const { filters, onChangeFilter } = useFilters();
+    const navigate = useNavigate();
 
     const lastIndex = currentPage * postsPerPage;
     const startingIndex = lastIndex - postsPerPage;
     let lastPage = Math.ceil(allPostsCount / postsPerPage);
 
     useEffect(() => {
-        postService
-            .getPostsCount()
-            .then(setAllPostsCount)
-            .catch((err) => console.log(err));
-    }, []);
+        const setPagination = async () => {
+            const postsCount = await postService.getPostsCount();
+            setAllPostsCount(postsCount);
 
-    useEffect(() => {
-        postService
-            .getPaginatedPosts(startingIndex, postsPerPage, filters)
-            .then(setPosts)
-            .catch((err) => console.log(err));
+            const paginatedPosts = await postService.getPaginatedPosts(
+                startingIndex,
+                postsPerPage,
+                filters
+            );
+            setPosts(paginatedPosts);
+        };
+
+        setPagination().catch((err) => {
+            logErrors(err);
+            navigate("/error");
+        });
     }, [currentPage, filters]);
 
     const decreasePageNumber = () => {
